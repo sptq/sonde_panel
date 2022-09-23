53479fc7e0a0e0cafa7cc44fc07099c78646ac5c
Marcin Grochulski <marcin@grochulski.net>
2022-09-23 14:25:27 +0200

    Fix sonde page

--- web/src/components/Paperbase.js
+++ web/src/components/Paperbase.js
@@ -197,7 +197,7 @@ export default function Paperbase() {
         </Box>
         <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
           <Header onDrawerToggle={handleDrawerToggle} />
-          <Box component="main" sx={{ flex: 1, bgcolor: '#eaeff1' }}>
+          <Box component="main" sx={{ flex: 1, py: 6, px: 4, bgcolor: '#eaeff1' }}>
             <Content />
           </Box>
           <Box component="footer" sx={{ p: 2, bgcolor: '#eaeff1' }}>
