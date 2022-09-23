35d65905340a237e1fca46dc83b14ebbe5e84d4e
Marcin Grochulski <marcin@grochulski.net>
2022-09-23 10:42:32 +0200

    Add static to backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SondeModule } from './sonde/sonde.module';
@@ -6,7 +9,11 @@ import { SdrModule } from './sdr/sdr.module';
 import { CfgModule } from './cfg/cfg.module';
 
 @Module({
-  imports: [SondeModule, SdrModule, CfgModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../web', 'build'),
+    }),
+    SondeModule, SdrModule, CfgModule],
   controllers: [AppController],
   providers: [AppService],
 })
