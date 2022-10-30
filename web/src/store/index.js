ed1cf277ecdcb84e1456e967cb71bd492145c4b8
Marcin Grochulski <marcin@grochulski.net>
2022-10-30 23:53:56 +0100

    Add persist store

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,5 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import { save, load } from "redux-localstorage-simple"
 import userReducer from "./userReducer";
+
 export default configureStore({
   reducer: {user: userReducer},
+  middleware: [save()],
+  preloadedState: load(),
 })
